074792a911ac887e29300d29dd08d5b32bdf6304
unknown <drgimenez@gmail.com>
2021-09-11 16:39:21 -0300

    Incentives excercise 2 finished

--- incentives/incentives.js
+++ incentives/incentives.js
@@ -3,6 +3,7 @@
 var path = require("path");
 var fs = require("fs");
 var crypto = require("crypto");
+const { Console } = require("console");
 
 const KEYS_DIR = path.join(__dirname,"keys");
 const PUB_KEY_TEXT = fs.readFileSync(path.join(KEYS_DIR,"pub.pgp.key"),"utf8");
@@ -47,14 +48,74 @@ countMyEarnings();
 
 function addPoem() {
 	// TODO: add lines of poem as transactions to the transaction-pool
+	for(let line of poem){
+		transactionPool.push(createTransaction(line));
+	}
 }
 
 function processPool() {
 	// TODO: process the transaction-pool in order of highest fees
+	let tx;
+	let txToProcess;
+	let blockTransactions = {
+		blockFee: {
+			blockFee: blockFee,
+			amount: 0,
+			account: PUB_KEY_TEXT
+		},
+		transactions: []
+	}
+
+	while(transactionPool.length > 0){
+		txToProcess = {};
+		txToProcess.fee = 0;
+		for(tx of transactionPool){
+			if (tx.fee > txToProcess.fee){
+				txToProcess = tx;
+			}
+		}
+
+		transactionPool.splice(transactionPool.indexOf(txToProcess), 1);
+		blockTransactions.transactions.push(txToProcess);
+		blockTransactions.blockFee.amount += txToProcess.fee;
+		
+		if(blockTransactions.transactions.length === maxBlockSize){
+			Blockchain.blocks.push(createBlock(blockTransactions));
+			blockTransactions = {
+				blockFee: {
+					blockFee: blockFee,
+					amount: 0,
+					account: PUB_KEY_TEXT
+				},
+				transactions: []
+			}
+		}
+	}
+
+	if(blockTransactions.transactions.length > 0) {
+		Blockchain.blocks.push(createBlock(blockTransactions));
+	}
 }
 
 function countMyEarnings() {
 	// TODO: count up block-fees and transaction-fees
+	let totalEarnings = 0;
+	let totalTransactions = 0;
+	for(let block of Blockchain.blocks){
+		if(block.index > 0){
+			totalEarnings += block.data.blockFee.amount
+						+ block.data.blockFee.blockFee;
+			totalTransactions += block.data.transactions.length;
+
+			console.log("---------------------------------------");
+			console.log("Block number:", block.index);
+			console.log("---------------------------------------");
+			console.log(block.data.transactions);
+		}
+	}
+	console.log("Total earnings:", totalEarnings);
+	console.log("Total Blocks:", Blockchain.blocks.length);
+	console.log("Total Transactions:", totalTransactions);
 }
 
 function createBlock(data) {
@@ -96,7 +157,8 @@ function createTransaction(data) {
 	};
 
 	tr.hash = transactionHash(tr);
-
+	// Random integer from 1 to 10:
+	tr.fee = Math.floor(Math.random() * 10) + 1;
 	return tr;
 }
 
